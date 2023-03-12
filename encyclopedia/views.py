93e3c78f73090ba85abafe10aeebbb0ba5763b12
Federico Perez Cusse <fepc94@gmail.com>
2023-03-12 13:17:00 -0300

    title of entrypage auto added

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,5 +8,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def entry(request):
-    return render(request, "encyclopedia/entry.html")
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "title" : title 
+    })
