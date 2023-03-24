563b8ee9ecc843b8bb8b698356658ca7d16d731d
Federico Perez Cusse <fepc94@gmail.com>
2023-03-24 09:17:53 -0300

    initial styling done New Entry form

--- encyclopedia/forms.py
+++ encyclopedia/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
 class NewEntryForm(forms.Form):
-    title = forms.CharField(label="Entry Title", max_length=100)
-    content = forms.CharField(widget=forms.Textarea(attrs={"rows":5}), label="Content")
-
+    title = forms.CharField(label="Entry Title", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 75%;'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={"rows":5, 'class': 'form-control', 'style': 'width: 75%;'}), label="Content")
 
